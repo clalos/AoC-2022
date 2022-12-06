b0e07f27682f531d9ea2b5c33c87196cc62a33a3
Claudio Loseto <loseto@pm.me>
2022-12-06 10:46:27 +0100

    Add day 6

---
+++ day6a/main.zig
@@ -0,0 +1,25 @@
+// https://adventofcode.com/2022/day/6
+
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const input = @embedFile("input.txt");
+const print = std.debug.print;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var unique_chars = std.AutoHashMap(u8, void).init(gpa.allocator());
+
+    var idx: u32 = 4;
+    while (idx < input.len) : (idx += 1) {
+        var chunk = input[idx - 4 .. idx];
+        try unique_chars.put(chunk[3], {});
+        try unique_chars.put(chunk[2], {});
+        try unique_chars.put(chunk[1], {});
+        try unique_chars.put(chunk[0], {});
+        if (unique_chars.count() == 4) {
+            print("First marker after character {d}\n", .{idx});
+            break;
+        }
+        unique_chars.clearAndFree();
+    }
+}
---
+++ day6b/main.zig
@@ -0,0 +1,26 @@
+// https://adventofcode.com/2022/day/6
+
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const input = @embedFile("input.txt");
+const print = std.debug.print;
+const marker_size = 14;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var unique_chars = std.AutoHashMap(u8, void).init(gpa.allocator());
+
+    var idx: u32 = marker_size;
+    while (idx < input.len) : (idx += 1) {
+        var chunk = input[idx - marker_size .. idx];
+        var char_count: u32 = marker_size;
+        while (char_count > 0) : (char_count -= 1) {
+            try unique_chars.put(chunk[char_count - 1], {});
+        }
+        if (unique_chars.count() == marker_size) {
+            print("First marker after character {d}\n", .{idx});
+            break;
+        }
+        unique_chars.clearAndFree();
+    }
+}
